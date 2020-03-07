6fe35a88d64929d111bf4d17dd48a27998f93f8a
admonguadalest <admonguadalest@gmail.com>
2020-03-07 16:53:20 +0100

    no comments

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from dataloader import CocoPoseDataset
 from torch.utils.data import *
 from helpers import *
 import cv2
+from multipersonparsing import performmultiparsing
+from datapreparation.SL_Fields import createconfidencemapsforpartaffinityfields, createconfidencemapsforpartdetection
 
 imagepath = '/home/carlos/PycharmProjects/PublicDatasets/Coco/val2017'
 annotationpath = '/home/carlos/PycharmProjects/PublicDatasets/Coco/annotations/person_keypoints_val2017.json'
@@ -10,11 +12,3 @@ dataset = CocoPoseDataset(imagepath, annotationpath, None)
 
 dataloader = DataLoader(dataset, batch_size=1)
 
-for i, o in enumerate(dataloader):
-    (im, ann, imwann) = o
-    imwann_ = imwann.clone().detach().numpy()
-    cv2.imshow('w', imwann_[0])
-    cv2.waitKey(1000)
-
-
-y = 2
